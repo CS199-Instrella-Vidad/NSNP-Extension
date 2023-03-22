a0516288148e75b4024b76364ff854c10d3c0749
reannu123 <reannumon123@gmail.com>
2023-03-22 11:39:53 +0800

    Deleted unneeded files

--- src/components/SideBar/SideBar.tsx
+++
@@ -1,9 +0,0 @@
-import React, { useState } from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
-import bootstrap from "bootstrap";
-
-function ModalExample() {
-  return <></>;
-}
-
-export default ModalExample;
