cf7b31f015744557de01ce751ad74d60e9708987
reannu123 <reannumon123@gmail.com>
2023-05-05 19:26:00 +0800

    Resolved comments

--- src/App.jsx
+++ src/App.jsx
@@ -10,11 +10,7 @@ function App() {
   return (
     <>
       <div className="App">
-        {/* <Header /> */}
-        {/* <ConfigHist /> */}
         <NSNP />
-        {/* <Simulation /> */}
-        {/* <Graph /> */}
       </div>
     </>
   );
