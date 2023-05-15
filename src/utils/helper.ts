2eecb97354d052e9f13facd52b4fb8d01e1178a2
reannu123 <reannumon123@gmail.com>
2023-05-15 23:37:28 +0800

    changed display of funcs and vars nums per neuron

--- src/utils/helper.ts
+++ src/utils/helper.ts
@@ -2,17 +2,25 @@ import { env } from "process";
 
 function functionToString(F: number[], j: number, vars, threshold) {
   let string = "";
-
-  string += "f_" + (j + 1) + "(";
+  console.log("Vars", vars);
+  string += "f_{" + j + "}(";
   for (let i = 0; i < vars.length; i++) {
-    string += "x_" + (vars[i] + 1) + ", ";
+    string += "x_" + (i + 1) + ", ";
   }
   string = string.slice(0, -2);
   string += ") = ";
+  let varCounter = 1;
   for (let i = 0; i < F.length; i++) {
+    // TODO: Instead of checking if each element is 1, cross reference Function Location and Variable Location to see if the variable is in the function
+
     if (F[i] != 0) {
       string +=
-        (F[i] > 1 ? F[i] : F[i] < 1 ? F[i] : "") + "x_" + (i + 1) + " + ";
+        (F[i] > 1 ? F[i] : F[i] < 1 ? F[i] : "") + "x_{" + varCounter + "} + ";
+      varCounter++;
+    } else {
+      if (i === vars[varCounter - 1]) {
+        varCounter++;
+      }
     }
   }
   string = string.slice(0, -3);
@@ -22,6 +30,7 @@ function functionToString(F: number[], j: number, vars, threshold) {
   if (threshold != 0) {
     string += "\\,|_" + threshold;
   }
+  console.log(string);
   return string;
 }
 
@@ -32,11 +41,13 @@ export function createNeuron(VL, C, F, L, i, T) {
 
   // Get the values of the variables
   let varString = "";
+  let varCounter = 1;
   for (let j = 0; j < VL.length; j++) {
     if (VL[j] === i + 1) {
-      varString += "x_" + (j + 1) + "[" + C[j] + "], \\;";
+      varString += "x_{" + varCounter + "}[" + C[j] + "], \\;";
       vars.push(C[j]);
       indices.push(j);
+      varCounter++;
     }
   }
   varString = varString.slice(0, -4);
@@ -44,6 +55,8 @@ export function createNeuron(VL, C, F, L, i, T) {
 
   let functionString = "";
   // Get the functions of the neuron
+
+  let funCounter = 1;
   for (let j = 0; j < L.length; j++) {
     if (L[j][i] === 1) {
       let threshold: number = 0;
@@ -51,7 +64,9 @@ export function createNeuron(VL, C, F, L, i, T) {
         if (T[k][0] === j + 1) threshold = T[k][1];
       }
 
-      functionString += functionToString(F[j], j, indices, threshold) + "\\\\";
+      functionString +=
+        functionToString(F[j], funCounter, indices, threshold) + "\\\\";
+      funCounter++;
     }
   }
   neuronText += functionString + "}";
